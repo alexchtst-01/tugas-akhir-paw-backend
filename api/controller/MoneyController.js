7c760a51cee9a4e916b98e5653f8862ed4c13b87
alexchtst-01 <acinatra@gmail.com>
2024-11-12 13:31:23 +0700

    feat: finished the getAccumulativeBalance controller

--- api/controller/MoneyController.js
+++ api/controller/MoneyController.js
@@ -1,7 +1,7 @@
 import Money from "../model/MoneyModel.js";
 import Expanse from "../model/ExpenseModel.js";
 
-function fufufafa(data) {
+function fufufafaSummary(data) {
   let temp = {};
 
   for (let item of data) {
@@ -16,11 +16,44 @@ function fufufafa(data) {
       temp[time] += item.total;
     }
   }
-  const result = Object.keys(temp).map((time) => ({
-    date: time,
-    totalMoney: temp[time],
-  }));
-  return result;
+  return temp;
+}
+
+function fufufafaCategory(data) {
+  let temp = {};
+
+  for (let item of data) {
+    // kalo ada reimbuse jadi nol aja kan di balikin uangnya
+    if (!temp[item.category]) {
+      if (item.reimbuse) {
+        temp[item.category] = 0;
+      } else {
+        temp[item.category] = item.total;
+      }
+    } else {
+      if (item.reimbuse) {
+        temp[item.category] += 0;
+      } else {
+        temp[item.category] += item.total;
+      }
+    }
+  }
+  return temp;
+}
+
+function fufufafaMoney(data) {
+  let temp = {
+    total_balance: 0,
+    total_income: 0,
+    total_expanse: 0,
+  };
+  for (let item of data) {
+    temp["total_balance"] += item.balance;
+    temp["total_income"] += item.total_income;
+    temp["total_expanse"] += item.total_expanse;
+  }
+
+  return temp;
 }
 
 export const justPostMoney = async (req, res) => {
@@ -36,13 +69,24 @@ export const justPostMoney = async (req, res) => {
 export const getAccumulativeBalance = async (req, res) => {
   const id = req.params.id;
   try {
-    const userMoneyData = await Expanse.find({ userID: id }, "total date");
+    const expanse = await Expanse.find(
+      { userID: id },
+      "total date category reimbuse"
+    );
+    const userMoney = await Money.find(
+      { userID: req.params.id },
+      "balance total_income total_expanse userID"
+    );
 
-    const accumulative = await fufufafa(userMoneyData);
+    const summary = await fufufafaSummary(expanse);
+    const accMoney = await fufufafaMoney(userMoney);
+    const category = await fufufafaCategory(expanse);
+
     res.status(200).json({
       msg: "data berhasil di retrieve",
-      money: userMoneyData,
-      acc: accumulative,
+      money: accMoney,
+      summary: summary,
+      budget: category,
     });
   } catch (error) {
     res.status(500).json({
