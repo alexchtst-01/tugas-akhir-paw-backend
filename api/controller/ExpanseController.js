bb119af13e4a357d722739722edc2e5b4d957768
alexchtst-01 <acinatra@gmail.com>
2024-11-22 10:36:04 +0700

    minor change

--- api/controller/ExpanseController.js
+++ api/controller/ExpanseController.js
@@ -4,18 +4,31 @@ import { google } from "googleapis";
 import fs from "fs";
 import path from "path";
 import mime from "mime";
+import dotenv from "dotenv";
 
-const KEYFILEPATH = "../../image-442016-7b27065ce6ab.json";
+dotenv.config();
+
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: KEYFILEPATH,
+  credentials: {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  },
   scopes: SCOPES,
 });
 
 const uploadImage = async (imgPath) => {
   const drive = google.drive({ version: "v3", auth });
-  const folderID = "1sOdj-GpvHxVjZq7BVclxbccLJtMb0XhQ";
+  const folderID = process.env.DRIVE_FOLDER_ID;
 
   try {
     const mimeType = mime.getType(imgPath) || "application/octet-stream";``
