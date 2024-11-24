8feeddaa9272b5a775c8f2affda740da945b3850
ryanlukito <ryanlukito13@gmail.com>
2024-11-24 10:20:07 +0700

    fix

--- api/controller/AuthenticationController.js
+++ api/controller/AuthenticationController.js
@@ -38,7 +38,7 @@ export const login = async (req, res) => {
     });
     return res.status(200).json({ msg: "berhasil login", token });
   } catch (error) {
-    res.status(500).json({ msg: `terjadi error ${error}` });
+    res.status(500).json({ msg: `terjadi error bang ${error}` });
   }
 };
 
